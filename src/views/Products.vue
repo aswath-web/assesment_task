b47c556d1859a3c97ee3f1109eea11e90565607f
aswath_web <aswath2003.kgm@gmail.com>
2023-07-06 18:46:31 +0530

    first commit

---
+++ src/views/Products.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <!-- <empform/> -->
+        <productTable />
+    </div>
+</template>
+<script>
+import productTable from '../components/productTable.vue'
+export default {
+    components: {
+        productTable
+    }
+}
+</script>
